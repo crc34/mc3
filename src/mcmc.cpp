02268bb90056203ea5d15325a2095c38a05837b3
user <user@email.com>
2017-04-09 16:28:03 -0400

    make rng thread local

--- src/mcmc.cpp
+++ src/mcmc.cpp
@@ -1,12 +1,12 @@
 #include "mcmc.h"
 #include <random>
 #include <math.h>
-#include <iostream>
-std::default_random_engine generator;
-std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+thread_local std::default_random_engine generator;
 
 bool accept(const double propLL, const double currentLL)
 {
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
     const double draw = log(distribution(generator));
     return draw < propLL - currentLL;
 }
