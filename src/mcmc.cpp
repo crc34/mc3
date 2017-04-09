015cd462af2be736fe8ef4302aed2224191a067e
user <user@email.com>
2017-04-09 16:51:38 -0400

    misc cleanup

--- src/mcmc.cpp
+++ src/mcmc.cpp
@@ -3,10 +3,10 @@
 #include <math.h>
 
 thread_local std::default_random_engine generator;
+std::uniform_real_distribution<double> stdUnif(0.0, 1.0);
 
 bool accept(const double propLL, const double currentLL)
 {
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    const double draw = log(distribution(generator));
+    const double draw = log(stdUnif(generator));
     return draw < propLL - currentLL;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,7 @@
 #include <catch.hpp>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 #include "mcmc.h"
 
 using std::vector;
@@ -11,13 +12,11 @@ TEST_CASE("accept", "[]")
 {
     const double currentProb = 0.5;
     const double proposalProb = 0.4;
-    double nAccepts = 0;
     const double n = 10000;
-    for(int i = 0; i < n; i++)
-    {
-        nAccepts += accept(log(proposalProb),  (currentProb));
-    }
-    nAccepts /= n;
+    vector<double> draws(n);
+    std::transform(draws.cbegin(), draws.cend(), draws.begin(),
+            [=](const double x){return  accept(log(proposalProb),  (currentProb));} );
+    const double nAccepts = std::accumulate(draws.cbegin(), draws.cend(), 0) / n;
     REQUIRE(abs(nAccepts - proposalProb/currentProb) < 0.05);
 }
 
