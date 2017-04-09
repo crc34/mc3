f1e68e11682bb2db40c01229f8e3bf0058acbd44
user <user@email.com>
2017-04-09 16:13:34 -0400

    test accept

--- src/mcmc.cpp
+++ src/mcmc.cpp
@@ -1,10 +1,12 @@
 #include "mcmc.h"
 #include <random>
-
+#include <math.h>
+#include <iostream>
 std::default_random_engine generator;
 std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
 bool accept(const double propLL, const double currentLL)
 {
-    return distribution(generator) < propLL - currentLL;
+    const double draw = log(distribution(generator));
+    return draw < propLL - currentLL;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,12 +1,24 @@
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <vector>
+#include <math.h>
 #include "mcmc.h"
+
+using std::vector;
 
 TEST_CASE("accept", "[]")
 {
-    REQUIRE(accept(-1.0, -2.0));
-
+    const double currentProb = 0.5;
+    const double proposalProb = 0.4;
+    double nAccepts = 0;
+    const double n = 10000;
+    for(int i = 0; i < n; i++)
+    {
+        nAccepts += accept(log(proposalProb),  (currentProb));
+    }
+    nAccepts /= n;
+    REQUIRE(abs(nAccepts - proposalProb/currentProb) < 0.05);
 }
 
 
